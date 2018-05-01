1c7e1e581d54d6de4c76a0ecf58e6c80f59947e2
Patrick Cullen <patrick47cullen@gmail.com>
2018-05-01 21:13:40 +0100

    Preparing the HashMap

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,11 +8,22 @@ public class LibraryTest {
 
     Library library;
     Bookn bookn;
+    Bookn booka;
+    Bookn bookb;
+    Bookn bookc;
+    Bookn bookd;
+    Bookn booke;
 
     @Before
     public void before(){
-        library = new Library(5);
+        library = new Library(6);
         bookn = new Bookn("Evil Dead", "Horror");
+        booka = new Bookn("The Devil Rides", "Horror");
+        bookb = new Bookn("Shane", "Western");
+        bookc = new Bookn("Creeping Vines", "Gardening");
+        bookd = new Bookn("Rangers and Celtic", "Sport");
+        booke = new Bookn("Dawn of Evil", "Horror");
+
     }
 
     @Test
@@ -22,33 +33,34 @@ public class LibraryTest {
 
     @Test
     public void getCapacity(){
-        assertEquals(5, library.getCapacity());
+        assertEquals(6, library.getCapacity());
     }
 
     @Test
     public void canAddBook() {
         library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
+        library.addBook(booka);
+        library.addBook(bookb);
+        library.addBook(bookc);
         assertEquals(4, library.bookCount());
     }
 
     @Test
     public void canGetCapacity() {
-        assertEquals(5, library.getCapacity());
+        assertEquals(6, library.getCapacity());
     }
 
     @Test
-    public void camAddBook__overcapacity() {
+    public void canAddBook__overcapacity() {
         library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
-        library.addBook(bookn);
-        assertEquals(5, library.bookCount());
+        library.addBook(booka);
+        library.addBook(bookb);
+        library.addBook(bookc);
+        library.addBook(bookd);
+        library.addBook(booke);
+        library.addBook(booke);
+        library.addBook(booke);
+        assertEquals(6, library.bookCount());
     }
-
 
 }
